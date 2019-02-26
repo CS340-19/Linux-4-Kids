a70925d1aaca8173045527eac1e296d4337fdcc1
jsamar1 <jsamar3@gmail.com>
2019-02-26 14:38:30 -0500

    feat: Actually working components

--- linux4Kids/src/app/app.module.ts
+++ linux4Kids/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
